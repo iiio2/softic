ab802f96db3ff57cede7efc1252234a022a7146b
Mr. Robot <gomexxxx71@gmail.com>
2022-12-22 17:56:53 +0600

    refactored code in post.tsx for post and user.

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -4,37 +4,45 @@ import {
   useGetCommentsByIdQuery,
   useGetUserQuery,
 } from "../slices/postApiSlice";
+import User from "../models/User";
+import Post from "../models/Post";
 
 const Post = () => {
   const { id, userId } = useParams();
-  const { data: post = [], isLoading, isError } = useGetPostByIdQuery(id || "");
-  const { data: comments = [] } = useGetCommentsByIdQuery(id || "");
+  const {
+    data: postData = [],
+    isLoading,
+    isError,
+  } = useGetPostByIdQuery(id as string);
+  const { data: comments = [] } = useGetCommentsByIdQuery(id as string);
 
-  const { data: user, isError: isUserError } = useGetUserQuery(userId || "");
+  const { data: userData = [], isError: isUserError } = useGetUserQuery(
+    userId as string
+  );
+
+  const getData = () => {
+    const post = postData as Post;
+    const user = userData as User;
+    return { post, user };
+  };
 
   if (isLoading) return <p>Loading...</p>;
-
   if (isError || isUserError) return <p>Error!</p>;
-
   return (
     <>
-      {post && "title" in post && user && "name" in user && (
-        <>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-          <h4> written by - {user.name}</h4>
-          <h3>Comments</h3>
-          <ul>
-            {comments.map((comment) => (
-              <div key={comment.id} className="comments">
-                <li>Name - {comment.name}</li>
-                <li>Email - {comment.email}</li>
-                <li>Body - {comment.body}</li>
-              </div>
-            ))}
-          </ul>
-        </>
-      )}
+      <h3>{getData().post.title}</h3>
+      <p>{getData().post.body}</p>
+      <h4> written by - {getData().user.name}</h4>
+      <h3>Comments</h3>
+      <ul>
+        {comments.map((comment) => (
+          <div key={comment.id} className="comments">
+            <li>Name - {comment.name}</li>
+            <li>Email - {comment.email}</li>
+            <li>Body - {comment.body}</li>
+          </div>
+        ))}
+      </ul>
     </>
   );
 };
