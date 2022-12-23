07f3ebac498b7134805017949f07b70b8f0e654c
Mr. Robot <gomexxxx71@gmail.com>
2022-12-24 00:07:36 +0600

    refactor post.tsx

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,45 +1,38 @@
+import _ from "lodash";
 import { useParams } from "react-router-dom";
 import {
   useGetPostByIdQuery,
   useGetCommentsByIdQuery,
   useGetUserQuery,
 } from "../slices/postApiSlice";
-import User from "../models/User";
-import Post from "../models/Post";
+import Column from "../models/Column";
 
 const Post = () => {
   const { id, userId } = useParams();
-  const {
-    data: postData = [],
-    isLoading,
-    isError,
-  } = useGetPostByIdQuery(id as string);
-  const { data: comments = [] } = useGetCommentsByIdQuery(id as string);
-
-  const { data: userData = [], isError: isUserError } = useGetUserQuery(
-    userId as string
-  );
-
-  const getData = () => {
-    const post = postData as Post;
-    const user = userData as User;
-    return { post, user };
-  };
+  const columns: Column[] = [
+    { key: 1, path: "name" },
+    { key: 2, path: "email" },
+    { key: 3, path: "body" },
+  ];
+  const { data: post, isLoading, isError } = useGetPostByIdQuery(id!);
+  const { data: comments = [] } = useGetCommentsByIdQuery(id!);
+  const { data: user, isError: isUserError } = useGetUserQuery(userId!);
 
   if (isLoading) return <p>Loading...</p>;
   if (isError || isUserError) return <p>Error!</p>;
+
   return (
     <>
-      <h3>{getData().post.title}</h3>
-      <p>{getData().post.body}</p>
-      <h4> written by - {getData().user.name}</h4>
+      <h3>{_.get(post, ["title"])}</h3>
+      <p>{_.get(post, ["body"])}</p>
+      <h4> written by - {_.get(user, ["name"])}</h4>
       <h3>Comments</h3>
       <ul>
         {comments.map((comment) => (
           <div key={comment.id} className="comments">
-            <li>Name - {comment.name}</li>
-            <li>Email - {comment.email}</li>
-            <li>Body - {comment.body}</li>
+            {columns.map((column) => (
+              <li key={column.path}>{_.get(comment, [column.path])}</li>
+            ))}
           </div>
         ))}
       </ul>
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,9 +1,12 @@
+import _ from "lodash";
 import { Link } from "react-router-dom";
 import { useGetPostsQuery } from "../slices/postApiSlice";
 
 const Posts = () => {
-  const { data: posts = [], isLoading } = useGetPostsQuery();
+  const { data: posts = [], isLoading, isError } = useGetPostsQuery();
+
   if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error!</p>;
 
   return (
     <div className="posts">
@@ -12,7 +15,7 @@ const Posts = () => {
         {posts.map((post) => (
           <div key={post.id}>
             <Link to={`/post/${post.id}/user/${post.userId}`}>
-              <li>{post.title}</li>
+              <li>{_.get(post, ["title"])}</li>
             </Link>
           </div>
         ))}
