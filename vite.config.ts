ae39e4dff01738d6d30e4b01544e738bd03f64b8
Mr. Robot <gomexxxx71@gmail.com>
2023-01-17 12:45:00 +0600

    change config.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   // server: {
   //   host: true,
-  //   port: 3000,
+  //   port: 8888,
   // },
   plugins: [react()],
 });
